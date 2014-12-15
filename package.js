94ad77a4ec87ed8b392db63f077b60badd7024ed
John Griffiths <john@uxgent.co>
2014-12-14 22:26:04 -0500

    Bump version to 0.0.2.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'johnantoni:meteor-skeleton',
   summary: 'Skeleton: A dead simple, responsive boilerplate, built for Meteor.',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/johnantoni/meteor-skeleton.git'
 });
 
