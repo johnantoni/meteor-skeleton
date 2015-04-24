09a745b1271b7fef6b9ea4e164a8111157fd8e38
John Griffiths <john@uxgent.co>
2015-04-24 11:42:23 -0400

    Update Skeleton.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'johnantoni:meteor-skeleton',
   summary: 'Skeleton: A dead simple, responsive boilerplate, built for Meteor.',
-  version: '0.0.3',
+  version: '0.0.4',
   git: 'https://github.com/johnantoni/meteor-skeleton.git'
 });
 
